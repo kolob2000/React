83ab9f7fb5801d39632fe0ba95722bf22bf938fb
Nick <k.kolabis@mail.ru>
2022-11-16 14:53:12 +0300

    edits

--- src/components/Messanger/Chatboard/Chatboard.js
+++ src/components/Messanger/Chatboard/Chatboard.js
@@ -31,11 +31,14 @@ export default () => {
             <input type="search" className="message_search" placeholder="Найти в Сообщении" id="message_search"/>
 
         </div>
-        <div className="chats_list">
-            {Object.entries(chatList).map(item => <ChatItem key={item[0]} roomId={item[0]} chatItem={item[1]}/>)}
+        <div className="chat_list_wrapper">
+            <div className="chats_list">
+                {Object.entries(chatList).reverse().map(item => <ChatItem key={item[0]} roomId={item[0]}
+                                                                          chatItem={item[1]}/>)}
+            </div>
         </div>
         <div className="chat_menu">
-            <button className="chat_menu__item" >
+            <button className="chat_menu__item">
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="14" viewBox="0 0 22 14" fill="">
                     <path fillRule="evenodd" clipRule="evenodd"
                           d="M15 6C16.66 6 17.99 4.66 17.99 3C17.99 1.34 16.66 0 15 0C13.34 0 12 1.34 12 3C12 4.66 13.34 6 15 6ZM7 6C8.66 6 9.99 4.66 9.99 3C9.99 1.34 8.66 0 7 0C5.34 0 4 1.34 4 3C4 4.66 5.34 6 7 6ZM7 8C4.67 8 0 9.17 0 11.5V13C0 13.55 0.45 14 1 14H13C13.55 14 14 13.55 14 13V11.5C14 9.17 9.33 8 7 8ZM15 8C14.71 8 14.38 8.02 14.03 8.05C14.05 8.06 14.06 8.08 14.07 8.09C15.21 8.92 16 10.03 16 11.5V13C16 13.35 15.93 13.69 15.82 14H21C21.55 14 22 13.55 22 13V11.5C22 9.17 17.33 8 15 8Z"
